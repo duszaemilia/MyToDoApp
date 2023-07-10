39237f4da9765e9c60760a12ff14e33a1574ec8c
duszaemilia <dusza.emilia@gmail.com>
2023-07-10 12:58:38 +0200

    posting data yo API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useEffect, useState} from "react";
+import {getAllTasks, sendTaskData} from "./helpers/api.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [task, setTask] = useState([]);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        getAllTasks()
+            .then((data) => {
+                setTask(data)
+            })
+            .catch(console.error)
+    }, [])
+
+
+
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const result = await sendTaskData({
+            title,description,status:'open',addedDate:new Date()
+        });
+      setTitle('');
+      setDescription('');
+      setTask([...task,result])
+    }
+
+    return (
+        <>
+            <form
+                onSubmit={handleSubmit}
+            >
+                <div>
+                    <label htmlFor="title">Title</label>
+                    <input
+                        value={title}
+                        type="text"
+                        id="title"
+                        name="title"
+                        onChange={(event) => setTitle(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="desc">Desription</label>
+                    <textarea
+                        value={description}
+                        id="desc"
+                        name="desc"
+                        onChange={(event) => setDescription(event.target.value)}
+                    />
+                </div>
+                <button type="submit">Add</button>
+            </form>
+        </>
+    )
 }
 
 export default App
